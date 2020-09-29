b0a49dd76666b3f75c37650ec6cec90aeeceb820
Ananya Gupta <ananya.it19@nsut.ac.in>
2020-09-30 00:18:46 +0530

    ..

--- AJAX-type ahead/AJAX-type ahead.js
+++ AJAX-type ahead/AJAX-type ahead.js
@@ -4,4 +4,41 @@ const cities = [];
 // Returns a promise
 fetch(endpoint)
   .then((blob) => blob.json())
-  .then((data) => cities.push(data));
+  .then((data) => cities.push(...data)); // ... used to change array into psuh method ( proper cities spreading)
+
+function findMatches(wordtoMatch, cities) {
+  return cities.filter((place) => {
+    // Find matrching state or cities
+    const regex = new RegExp(wordtoMatch, "gi"); // g -- global , i == case insensitive
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray
+    .map((place) => {
+      const regex = new RegExp(this.value, "gi");
+      const cname = place.city.replace(
+        regex,
+        `<span class="hl"> ${this.value}</span`
+      );
+      const sname = place.state.replace(
+        regex,
+        `<span class="hl"> ${this.value}</span`
+      );
+      return `
+    <li>
+        <span class= 'name'> ${cname} , ${sname} </span>
+        <span class="population'> ${place.population} </span>
+    </li>
+    `;
+    })
+    .join();
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
